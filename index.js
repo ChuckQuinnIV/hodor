b8a77f688d1537d15ff0789db7671c91b3971c77
Chuck Quinn IV <cquinniv@gmail.com>
2017-08-25 11:56:31 -0500

    Certification fixes

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
-const APP_ID = 'amzn1.ask.skill.c12c0383-89de-4c10-8d87-665dc2388f7b';
-
-const voice = {
-  SKILL_NAME: 'Hodor',
-  WELCOME_MESSAGE: 'Hodor',
-  WELCOME_REPROMPT: 'Hodor',
-  HELP_MESSAGE: 'Hodor',
-  STOP_MESSAGE: 'Hodor'
-};
 
 exports.handler = function(event, context, callback) {
   const alexa = Alexa.handler(event, context);
-  alexa.appId = APP_ID;
-  alexa.resources = voice;
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
 
 const handlers = {
   'LaunchRequest': function () {
-    this.attributes.speechOutput = voice.WELCOME_MESSAGE;
-    this.attributes.repromptSpeech = voice.WELCOME_REPROMPT;
-    this.emit(':tell', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    this.attributes.speechOutput = 'Hodor Hodor!';
+    this.emit(':tell', this.attributes.speechOutput);
   },
   'AMAZON.HelpIntent': function () {
-    this.attributes.speechOutput = voice.HELP_MESSAGE;
-    this.attributes.repromptSpeech = voice.HELP_REPROMPT;
-    this.emit(':tell', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    this.attributes.speechOutput = 'Ask Hodor anything.';
+    this.emit(':ask', this.attributes.speechOutput, this.attributes.speechOutput);
   },
   'AMAZON.RepeatIntent': function () {
-    this.emit(':tell', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    this.emit('SessionEndedRequest');
   },
   'AMAZON.StopIntent': function () {
     this.emit('SessionEndedRequest');
@@ -40,11 +27,11 @@ const handlers = {
     this.emit('SessionEndedRequest');
   },
   'SessionEndedRequest': function () {
-    this.emit(':tell', voice.STOP_MESSAGE);
+    this.attributes.speechOutput = 'Hodor';
+    this.emit(':tell', this.attributes.speechOutput);
   },
   'Unhandled': function () {
-    this.attributes.speechOutput = voice.HELP_MESSAGE;
-    this.attributes.repromptSpeech = voice.HELP_MESSAGE;
-    this.emit(':tell', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    this.attributes.speechOutput = 'Hodor';
+    this.emit(':tell', this.attributes.speechOutput);
   }
 };
